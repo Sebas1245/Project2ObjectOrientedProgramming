53d98bceb731a2e60ce5f8d993678d43ffb81e0f
Sebastián Saldaña <A01570274@itesm.mx>
2020-04-09 16:56:30 -0500

    Created example files and main.cpp file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "Funcion.h"
+#include "Hora.h"
+#include "Pelicula.h"
+#include "Actor.h"
+
+using namespace std;
+
+int main(){
+    Actor arrActores[20];
+    Funcion arrFunciones[20];
+    Pelicula arrPelis[20];
+    
+    return 0;
+}
