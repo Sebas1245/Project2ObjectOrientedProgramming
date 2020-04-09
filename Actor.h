54a6e34cab2406ea8b376278212f2412d7474643
Sebastián Saldaña <A01570274@itesm.mx>
2020-04-08 21:24:32 -0500

    Declared Actor and Pelicula classes

---
+++ Actor.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Actor {
+    private:
+        int id;
+        string nombre;
+    public:
+        Actor();
+        Actor(int num,string nom);
+        int getId();
+        string getNombre();
+        void setId(int num);
+        void setNombre(string nom);
+}
---
+++ Pelicula.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include "Actor.h"
+
+using namespace std;
+
+class Pelicula {
+    private:
+    int numPeli;
+    string titulo;
+    int anio;
+    int duracion;
+    string genero;
+    int cantActores;
+    Actor listaActores[10];
+    public:
+
+}
